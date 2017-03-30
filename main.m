3bc74e68e7591b91a7a0fee271f424b5488399ff
SinkableFish <nihongtao96@gmail.com>
2017-03-30 19:32:36 +0800

    refresh main

--- main.m
+++ main.m
@@ -5,12 +5,11 @@ test4 = imread('.\origin\4.bmp');
 test5 = imread('.\origin\5.bmp'); 
 test6 = imread('.\origin\6.jpg');
 test7 = imread('.\origin\7.tif');
-test8 = imread('.\origin\8.x-png');
-darkChannel(test4);
+test8 = imread('.\origin\8.x-png');%∞µÕ®µ¿≤‚ ‘Õº∆¨
+% cutAlphabet(test1);
+% gaussianNoise(test2);
 % medianFilter(test3);
-% histEqual(test5);
+% darkChannel(test4);
+histEqual(test5);
 % tempData6 = intrans(test6);
 % motionBlur(test7);
-% test1=rgb2gray(test1);
-% cutAlphabet(test1);
-    

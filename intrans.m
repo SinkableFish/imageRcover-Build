79f6aeb2d38f429c052722125c86a48cabf6ebcb
SinkableFish <nihongtao96@gmail.com>
2017-03-30 13:02:47 +0800

    Butterworth Filter to show bones

--- intrans.m
+++ intrans.m
@@ -1,4 +1,4 @@
-function [ targetData ] = intrans( data )
+function [ ghe ] = intrans( data )
 %对比度拉伸增强骨骼结构
 tempData = rgb2gray(data);
 % [~,~,k] = size(data);
@@ -10,12 +10,29 @@ tempData = rgb2gray(data);
 %     targetData(:,:,i) = tempData;
 % end
 % imshow(targetData,[0,255]);
-low_in = double(min(tempData(:)))./255;
-high_in = double(max(tempData(:)))./255;
-targetData = imadjust(tempData,[0,0.3],[0,1]);
-figure();
-imshow(targetData);
-imwrite(targetData,'.\results\6.png','png');
-    
+
+% low_in = double(min(tempData(:)))./255;
+% high_in = double(max(tempData(:)))./255;
+% targetData = imadjust(tempData,[0,0.4],[0,1]);
+% figure();
+% imshow(targetData);
+% imwrite(targetData,'.\results\6-1.png','png');
+
+
+PQ = size(tempData);
+D0 = 0.05*PQ(1);
+% HBW = lpfilter('btw', PQ(1), PQ(2), D0, 2); %书上的代码...实际巴特沃斯高通滤波器需要自行编写
+%巴特沃斯高通滤波器
+[U, V] = dftuv( PQ(1), PQ(2));
+D = hypot(U, V);
+HBW = 1 - 1./(1+(D./D0).^(2*PQ(2)));
+H = 0.5+0.01*HBW;
+gbw = dftfilt(tempData, HBW, 'fltpoint');
+gbw = gscale(gbw);
+ghf = dftfilt(tempData, H, 'fltpoint');
+ghf = gscale(ghf);
+ghe = histeq(ghf, 256);
+imshow(ghe);
+imwrite(ghe, '.\results\6-2.png', 'png');
 end
 

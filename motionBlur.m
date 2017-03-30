32f8460e706e5087246b42b5bd7ddbd6d02ea3e5
SinkableFish <nihongtao96@gmail.com>
2017-03-30 13:01:50 +0800

    reBuild the moniton blur

--- motionBlur.m
+++ motionBlur.m
@@ -1,15 +1,8 @@
 function [  ] = motionBlur( data )
-% tempData=rgb2gray(data);
-% PQ=size(tempData);
-% F = fft2(tempData, PQ(1), PQ(2));
-% data = rgb2gray(data);
-PSF = fspecial('motion',80,0);
-tempRecover = imfilter(data,PSF,'circular','conv');
-figure();
+%¶¯Ì¬Ä£ºý¸´Ô­
+PSF = fspecial('motion',8,0);
+tempRecover = deconvwnr(data, PSF, 0.02);
 imshow(tempRecover);
-tempRecover = deconvlucy(tempRecover,PSF,3);
-% tempRecover = deconvwnr(tempRecover, PSF);
-figure();
-imshow(tempRecover, []);
+imwrite(tempRecover, '.\results\7.png', 'png');
 end
 
